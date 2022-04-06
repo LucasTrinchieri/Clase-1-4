23a08ef571405b2159837f30f344654348707394
LucasTrinchieri <lucastrinchieri123@gmail.com>
2022-04-06 13:48:13 -0300

    Commit hasta el ejercicio 11

---
+++ NuevosConceptos/NuevosConceptos/Cliente.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuevosConceptos
+{
+    public class Cliente
+    {
+        public string Nombre { get; set; }
+        public uint CUIT { get; set; }
+        public double Saldo { get; set; }
+
+        public static Cliente operator -(Cliente primerCliente, Cliente segundoCliente)
+        {
+            Cliente clienteResultante = new Cliente();
+
+            if (primerCliente.CUIT == segundoCliente.CUIT)
+                clienteResultante.Saldo = primerCliente.Saldo - segundoCliente.Saldo;
+
+            clienteResultante.Saldo = primerCliente.CUIT == segundoCliente.CUIT ?
+                                      primerCliente.Saldo - segundoCliente.Saldo :
+                                      Math.Min(primerCliente.Saldo, segundoCliente.Saldo);
+
+            return clienteResultante;
+        }
+    }
+}
---
+++ NuevosConceptos/NuevosConceptos/Principal.cs
@@ -0,0 +1,88 @@
+﻿using System.Text.RegularExpressions;
+
+namespace NuevosConceptos
+{
+    public static class Principal
+    {
+        public static int SepararPalabras(this string stringIngresado) //1
+        {
+            return stringIngresado.Split(' ').Length;
+        }
+
+        public static int SepararPalabras(this string stringIngresado, char separador) //2
+        {
+            return stringIngresado.Split(separador).Length;
+        }
+
+        public static decimal DiferenciaEntreFechas(this DateTime fechaPrimera, DateTime fechaSegunda) //3
+        {
+            return (fechaPrimera.Second - fechaSegunda.Second) / 60;
+        }
+
+        public static double Promedio(this List<int> listaEnteros) //4
+        {
+            return listaEnteros.Average();
+        }
+        public static int MultiplicacionLista(this List<int> listaEnteros) //5
+        {
+            int pares = 0;
+            int impares = 0;
+
+            foreach (int entero in listaEnteros)
+            {
+                if(entero % 2 == 0)
+                {
+                    pares += entero;
+                }
+                else { impares -= entero; }
+            }
+
+            return pares * impares;
+        }
+
+        public static DateTime RetornarStringComoFecha(this string stringIngresado) //6
+        {
+            try
+            {
+                return DateTime.Parse(stringIngresado);
+            }
+            catch (Exception)
+            {
+                return DateTime.MinValue;
+            }
+        }
+
+        public static string Invertido(this string stringInvertido) //7
+        {
+            return stringInvertido.Reverse().ToString();
+        }
+
+        public static string UnionStrings(this string[] arrayString, char separadorChar) //8
+        {
+            return string.Join(separadorChar, arrayString);
+        }
+
+        public static bool EsCorreo(this string posibleCorreo) //9
+        {
+            string patron = @"^\w+[@]\w+[.]\w{2,}$";
+
+            return Regex.IsMatch(posibleCorreo, patron);
+        }
+
+        public static bool EsCUIT(this string posibleCUIT) //10
+        {
+            string patron = @"^\d{2}-\d+-\d$";
+
+            return Regex.IsMatch(posibleCUIT, patron);
+        }
+
+        //11
+
+        public static Cliente DevolverSaldoCliente(Cliente primerCliente, Cliente segundoCliente)
+        {
+            return primerCliente - segundoCliente;
+        }
+
+
+    }
+}
